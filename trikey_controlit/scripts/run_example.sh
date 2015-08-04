9b7bae24c520546f1a853b03592f2e275688781c
Chien-Liang Fok <liangfok@utexas.edu>
2015-08-04 15:56:12 -0500

    Got Trikey demos to work again.

--- trikey_controlit/scripts/run_example.sh
+++ trikey_controlit/scripts/run_example.sh
@@ -3,10 +3,10 @@
 # rosparam set /trikey_controller/command_delay_ms 200
 # rosparam set /trikey_controller/state_delay_ms 200
 
-rostopic pub -1 /check_delay_parameters std_msgs/Bool true
+#rostopic pub -1 /trikey_controller/check_delay_parameters std_msgs/Bool true
 
-rostopic pub -1 /embedded_kd std_msgs/Float64MultiArray '{layout: {dim: [{label: "x", size: 3, stride: 3}], data_offset: 0}, data: [0.0, 0.0, 0.0]}'
+# rostopic pub -1 /trikey_controller/embedded_kd std_msgs/Float64MultiArray '{layout: {dim: [{label: "x", size: 3, stride: 3}], data_offset: 0}, data: [0.0, 0.0, 0.0]}'
 
-rostopic pub -1 /trikey_controller/XYTask/goalPosition std_msgs/Float64MultiArray '{layout: {dim: [{label: "x", size: 3, stride: 3}], data_offset: 0}, data: [0.5, 0.0, 0.0]}'
-rostopic pub -1 /trikey_controller/ThetaTask/goalVector std_msgs/Float64MultiArray '{layout: {dim: [{label: "x", size: 3, stride: 3}], data_offset: 0}, data: [1.0, 1.0, 0.0]}'
+rostopic pub -1 /trikey_controller/XYTask/goalPosition std_msgs/Float64MultiArray '{layout: {dim: [{label: "x", size: 3, stride: 3}], data_offset: 0}, data: [0.5, 0.0, 0.0]}' &
+rostopic pub -1 /trikey_controller/HeadingTask/goalVector std_msgs/Float64MultiArray '{layout: {dim: [{label: "x", size: 3, stride: 3}], data_offset: 0}, data: [0.0, -1.0, 0.0]}'
 
